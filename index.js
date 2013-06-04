5f14daff0fc33a7f6634bf9006211cb4632f60cd
Žygimantas <zygimantas@soundest.lt>
2013-06-05 02:25:02 +0300

    initialize

--- index.js
+++ index.js
@@ -1,79 +1,59 @@
-var _ = require('underscore'),
-    Backbone = require('backbone');
-
-var Flow = Backbone.Model.extend({
-    addStep: function (callback) {
-        this._chain[this._chain.length] = callback;
-        return this;
+var Flow = function () {
+            this.id = 0;
+            this._chain = [];
+            this._currentStep = 0;
     },
+    flowIterator = 0,
+    flowStorage = {};
 
-    nextFrom: function (index, data) {
-        this._currentStep = index - 1;
-        this.next(data);
-    },
-
-    repeat: function (data) {
-        if (!_.isUndefined(this._chain[this._currentStep])) {
-            var me = this;
-            process.nextTick(function() {
-                me._chain[me._currentStep](me, data);
-            });
-        }
-    },
-
-    next: function (data) {
-        if (!_.isUndefined(this._chain[this._currentStep + 1])) {
-            this._currentStep += 1;
-            var me = this;
-            process.nextTick(function() {
-                me._chain[me._currentStep](me, data);
-            });
-        }
-    },
-
-    last: function(data) {
-        if(this._chain.length > 1 && this._chain.length - 1 != this._currentStep) {
-            this._currentStep = this._chain.length - 2;
-            this.next(data);
-        } else {
-            this.next(data);
-        }
-    },
-
-    getStep: function () {
-        return this._currentStep;
-    },
-
-    execute: function (data) {
-        if (_.isUndefined(data)) {
-            data = null;
-        }
-        this._currentStep = -1;
-        this.next(data);
+Flow.prototype.addStep = function (callback) {
+    this._chain[this._chain.length] = callback;
+    return this;
+};
+Flow.prototype.nextFrom = function (index, data) {
+    this._currentStep = index - 1;
+    this.next(data);
+};
+Flow.prototype.repeat = function (data) {
+    if (typeof this._chain[this._currentStep] !== 'undefined') {
+        var me = this;
+        process.nextTick(function() {
+            me._chain[me._currentStep](me, data);
+        });
     }
-});
-
-var FlowsManager = function () {
-    return {
-        _flows: new Backbone.Collection(),
-        create: function () {
-            var flow = new Flow();
-            flow._storage = new Backbone.Collection();
-            flow._chain = [];
-            flow._currentStep = 0;
-            this._flows.add({
-                id: flow.id,
-                flow: flow
-            });
-            return flow;
-        },
-        destroy: function (flow) {
-            var models = this._flows.where({id: flow.id});
-            if (models.length > 0) {
-                this._flows.remove(models);
-            }
-        }
-    };
+};
+Flow.prototype.next = function (data) {
+    if (typeof this._chain[this._currentStep + 1] !== 'undefined') {
+        this._currentStep += 1;
+        var me = this;
+        process.nextTick(function() {
+            me._chain[me._currentStep](me, data);
+        });
+    } else {
+        module.exports.destroy(this);
+    }
+};
+Flow.prototype.execute = function(data) {
+    if (typeof data === 'undefined') {
+        data = null;
+    }
+    this._currentStep = -1;
+    this.next(data);
 };
 
-module.exports = new FlowsManager();
+module.exports = {
+    create: function () {
+        var iterator = ++flowIterator;
+        flowStorage[iterator] = new Flow();
+        flowStorage[iterator].id = iterator;
+        return flowStorage[iterator];
+    },
+    destroy: function (flow) {
+        if (typeof flowStorage[flow.id] !== 'undefined') {
+            delete flowStorage[flow.id];
+        }
+    },
+    size: function() {
+        return Object.keys(flowStorage).length;
+    }
+};
---
+++ test.js
@@ -0,0 +1,115 @@
+var Flows = require('./index'),
+    assert = require('assert'),
+    Tests = {
+
+        simpleFlow: function (flow, data) {
+            Flows
+                .create()
+                .addStep(function (flow, data) {
+                    assert.strictEqual(data.param, 1);
+                    data.param += 1;
+                    flow.next(data);
+                })
+                .addStep(function (flow, data) {
+                    assert.strictEqual(data.param, 2);
+                    data.param += 1;
+                    flow.next(data);
+                })
+                .addStep(function (flow, data) {
+                    assert.strictEqual(data.param, 3);
+                    console.log('| ' + data.realFlowData.step + ' . Passed | Simple Flow');
+                    data.realFlowData.step += 1;
+                    data.realFlow.next(data.realFlowData);
+                    flow.next();
+                })
+                .execute({
+                    param: 1,
+                    realFlow: flow,
+                    realFlowData: data
+                });
+        },
+
+        repeatStep: function (flow, data) {
+            Flows
+                .create()
+                .addStep(function (flow, data) {
+                    data.param += 1;
+                    if (data.param < 3) {
+                        flow.repeat(data);
+                    } else {
+                        flow.next(data);
+                    }
+                })
+                .addStep(function (flow, data) {
+                    assert.strictEqual(data.param, 3);
+                    data.param += 1;
+                    flow.next(data);
+                })
+                .addStep(function (flow, data) {
+                    assert.strictEqual(data.param, 4);
+                    console.log('| ' + data.realFlowData.step + ' . Passed | Repeat Step');
+                    data.realFlowData.step += 1;
+                    data.realFlow.next(data.realFlowData);
+                    flow.next();
+                })
+                .execute({
+                    param: 1,
+                    realFlow: flow,
+                    realFlowData: data
+                });
+        },
+
+        skipStep: function (flow, data) {
+            Flows
+                .create()
+                .addStep(function (flow, data) {
+                    assert.strictEqual(data.param, 1);
+                    flow.nextFrom(2, data);
+                })
+                .addStep(function (flow, data) {
+                    data.param += 1;
+                    flow.next(data);
+                })
+                .addStep(function (flow, data) {
+                    assert.strictEqual(data.param, 1);
+                    console.log('| ' + data.realFlowData.step + ' . Passed | Skip Step');
+                    data.realFlowData.step += 1;
+                    data.realFlow.next(data.realFlowData);
+                    flow.next();
+                })
+                .execute({
+                    param: 1,
+                    realFlow: flow,
+                    realFlowData: data
+                });
+        }
+
+
+
+
+    }
+
+Flows
+    .create()
+    .addStep(function (flow) {
+        console.log('------------------------------------');
+        console.log('|              Testing             |');
+        console.log('------------------------------------');
+
+        flow.next({step : 1});
+    })
+    .addStep(Tests.simpleFlow)
+    .addStep(Tests.repeatStep)
+    .addStep(Tests.skipStep)
+    .addStep(function (flow) {
+        console.log('------------------------------------');
+        console.log('|          ALL TESTS PASSED        |');
+        console.log('------------------------------------');
+        flow.next();
+    })
+    .execute();
+setTimeout(function() {
+    console.log('size', Flows.size());
+}, 1000);
+
+
